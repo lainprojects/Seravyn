362de227f414a9c43da637df458b59e0aa38f475
lain <hlolkayla038@gmail.com>
2026-03-02 08:17:18 -0600

    Create RigManager.cs

---
+++ Classes/Menu/RigManager.cs
@@ -0,0 +1,85 @@
+using GorillaLocomotion;
+using HarmonyLib;
+using Photon.Pun;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Seravyn.Classes.Menu
+{
+    public static class RigManager
+    {
+
+        public static VRRig GetRandomVRRig(bool includeSelf)
+        {
+            VRRig random = GorillaParent.instance.vrrigs[UnityEngine.Random.Range(0, GorillaParent.instance.vrrigs.Count - 1)];
+            if (includeSelf)
+            {
+                return random;
+            }
+            else
+            {
+                if (random != GorillaTagger.Instance.offlineVRRig)
+                {
+                    return random;
+                }
+                else
+                {
+                    return GetRandomVRRig(includeSelf);
+                }
+            }
+        }
+
+        public static VRRig GetClosestVRRig()
+        {
+            float num = float.MaxValue;
+            VRRig outRig = null;
+            foreach (VRRig vrrig in GorillaParent.instance.vrrigs)
+            {
+                if (Vector3.Distance(GorillaTagger.Instance.bodyCollider.transform.position, vrrig.transform.position) < num)
+                {
+                    num = Vector3.Distance(GorillaTagger.Instance.bodyCollider.transform.position, vrrig.transform.position);
+                    outRig = vrrig;
+                }
+            }
+            return outRig;
+        }
+
+        public static PhotonView GetPhotonViewFromVRRig(VRRig p)
+        {
+            return (PhotonView)Traverse.Create(p).Field("photonView").GetValue();
+        }
+
+        public static Photon.Realtime.Player GetRandomPlayer(bool includeSelf)
+        {
+            if (includeSelf)
+            {
+                return PhotonNetwork.PlayerList[UnityEngine.Random.Range(0, PhotonNetwork.PlayerList.Length - 1)];
+            }
+            else
+            {
+                return PhotonNetwork.PlayerListOthers[UnityEngine.Random.Range(0, PhotonNetwork.PlayerListOthers.Length - 1)];
+            }
+        }
+
+        public static Photon.Realtime.Player GetPlayerFromVRRig(VRRig p)
+        {
+            return GetPhotonViewFromVRRig(p).Owner;
+        }
+
+        public static Photon.Realtime.Player GetPlayerFromID(string id)
+        {
+            Photon.Realtime.Player found = null;
+            foreach (Photon.Realtime.Player target in PhotonNetwork.PlayerList)
+            {
+                if (target.UserId == id)
+                {
+                    found = target;
+                    break;
+                }
+            }
+            return found;
+        }
+    }
+}
