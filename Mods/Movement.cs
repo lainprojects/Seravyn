6e323bd069d8864f46e6bff80f674772cc02e1b6
lain <hlolkayla038@gmail.com>
2026-03-03 07:41:37 -0600

    Remove Friend System. - From Platforms etc.

--- Mods/Movement.cs
+++ Mods/Movement.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * ii's Stupid Menu  Mods/Movement.cs
  * A mod menu for Gorilla Tag with over 1000+ mods
  *
@@ -200,8 +200,6 @@ namespace iiMenu.Mods
 
             if (Buttons.GetIndex("Non-Sticky Platforms").enabled)
                 platform.transform.position += right * ((left ? 1f : -1f) * ((0.025f + platform.transform.localScale.x / 2f) * (scaleWithPlayer ? GTPlayer.Instance.scale : 1f)));
-
-            FriendManager.PlatformSpawned(true, platform.transform.position, platform.transform.rotation, platform.transform.localScale, GetPlatformPrimitiveType());
         }
 
         public static int flySpeedCycle = 1;
@@ -255,8 +253,6 @@ namespace iiMenu.Mods
                             rightplat = null;
                         if (platformMode == 4 && rightplat == null)
                             UpdateClipColliders(true);
-
-                        FriendManager.PlatformDespawned(true);
                         break;
                     }
             }
