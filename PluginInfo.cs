b5332b62fc3a054705b9884e19855761265e2203
lain <hlolkayla038@gmail.com>
2026-03-03 19:03:30 -0600

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -29,7 +29,7 @@ namespace iiMenu
         public const string Name = "Seravyn Menu";
         public const string Description = "Created by @superiorspeech with love <3";
         public const string BuildTimestamp = "2026-03-2";
-        public const string Version = "1.0.5";
+        public const string Version = "1.1.0";
 
         public const string BaseDirectory = "Seravyn";
         public const string ClientResourcePath = "Seravyn.Resources.Client";
